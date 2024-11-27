19cb6862f1833f67ef508b22053f7c252324cc2d
whitewolf940701 <148895166+whitewolf940701@users.noreply.github.com>
2024-11-27 17:02:43 +0800

    行動端更新

--- js/p5.js
+++ js/p5.js
@@ -6,11 +6,12 @@ function setup() {
 
 function draw() {
 
-    if(mouseIsPressed){
+    if( mouseIsPressed && windowWidth>450 ){
 
         clear()
         for(i=0;i<3;i++){
-        stroke('#75F211')
+        stroke(255)
+        strokeWeight(1);
         line(mouseX, mouseY, mouseX+random(-20*PI,20*PI), mouseY+random(-20*PI,20*PI));
         line(mouseX, mouseY, mouseX+random(-50*PI,50*PI), mouseY+random(-50*PI,50*PI));
         }
@@ -21,10 +22,17 @@ function draw() {
         // circle(mouseX+random(-50,50), mouseY+random(-50,50),random(5))
         
         translate(mouseX,mouseY)
-        fill(117,242,17,255)
+        // fill(117,242,17,255)
+        fill(255)
         strokeWeight(0);
-        circle(0,0,35)
+        circle(0,0,25)
 
+        strokeWeight(2);
+        // stroke(117,242,17,255)
+        noFill()
+        circle(0,0,150)
+        // circle(0,0,50)
+        strokeWeight(0);
 
         for(i=0;i<180;i++){
         fill('#75F211')
@@ -38,6 +46,27 @@ function draw() {
         circle(mouseX*noise(0.005 * frameCount-10), mouseY*noise(0.005 * frameCount-10),random(3))
         }		
 
+
+    }else if(windowWidth<450){
+        
+        clear()
+        stroke('#75F211')
+        translate(mouseX,mouseY)
+        // fill(117,242,17,150)
+        fill(255)
+        strokeWeight(0);
+        circle(0,0,80)
+
+        stroke(255)
+        for(i=0;i<180;i++){
+        fill(255)
+        rotate(2)
+        circle(mouseX*noise(0.005 * frameCount), mouseY*noise(0.005 * frameCount),random(3))
+        }		
+        for(i=0;i<360;i++){
+        rotate(2)
+        circle(mouseX*noise(0.005 * frameCount-10), mouseY*noise(0.005 * frameCount-10),random(3))
+        }		
 
     }else{
 
@@ -55,9 +84,17 @@ function draw() {
         stroke('#75F211')
         translate(mouseX,mouseY)
         fill(117,242,17,150)
+        fill(255)
         strokeWeight(0);
-        circle(0,0,35)
+        circle(0,0,150)
 
+        // strokeWeight(30);
+        // // stroke(117,242,17,255)
+        // stroke(255)
+        // noFill()
+        // circle(0,0,150)
+
+        strokeWeight(0);
         stroke(255)
         for(i=0;i<180;i++){
         fill(255)
@@ -69,6 +106,8 @@ function draw() {
         circle(mouseX*noise(0.005 * frameCount-10), mouseY*noise(0.005 * frameCount-10),random(3))
         }		
     }
+
+
 		
 	
 }
--- js/today.js
+++ js/today.js
@@ -41,6 +41,19 @@ $(document).ready(function(){
 
     });
 
-
-
+    // Initialize Lenis
+    const lenis = new Lenis({
+    autoRaf: true,
+    });
+    
+    // Listen for the scroll event and log the event data
+    lenis.on('scroll', (e) => {
+    console.log(e);
+    });
+    
+    function toTop (){
+        lenis.scrollTo(0)
+    } 
+    
 });
+
